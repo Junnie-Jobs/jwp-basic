47b8ab99eb6db348b5ff2a95a27ff3a96c4a29c1
Junnie-Jobs <dj0999@naver.com>
2016-03-17 21:46:58 +0900

    Dao mission finished

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import core.jdbc.ConnectionManager;
 import next.model.User;
 
@@ -35,14 +37,79 @@ public class UserDao {
 		}
 	}
 	
-	public void update(User user) throws SQLException {
-		// TODO 구현 필요함.
+	public List<User> findAll() throws SQLException {
+		
+		//요구사항1
+		//SELECT userId, password, name, email FROM USERS -전체 목록 조회 쿼리
+		//목록 조회 while(rs.next()) {}
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<User> userlist = new ArrayList<User>();
+		try {
+			con = ConnectionManager.getConnection();
+			String sql = "SELECT userId, password, name, email FROM users";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				String userId = rs.getString("userId");
+				String password = rs.getString("password");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				
+				User user = new User(userId,password,name,email);
+				userlist.add(user);
+			}
+			
+		} finally {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			
+			if (con != null) {
+				con.close();
+			}
+		}
+		
+		return userlist;
+		
 	}
 	
-	public List<User> findAll() throws SQLException {
-		// TODO 구현 필요함.
-		return new ArrayList<User>();
-	}
+	
+	
+	public void update(User user) throws SQLException {
+		//요구사항2.
+		//로그인 사용자는 자신의 개인 정보를 수정할 수 있어야 한다.
+		//개인정보수정 쿼리 UPDATE USERS set password = ?, name = ?, email = ? WHERE userId = ?
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = ConnectionManager.getConnection();
+			String sql = "UPDATE USERS set password = ?, name = ?, email = ? WHERE userId = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, user.getPassword());
+			pstmt.setString(2, user.getName());
+			pstmt.setString(3, user.getEmail());
+			pstmt.setString(4, user.getUserId());
+			
+			pstmt.executeUpdate();
+		} finally {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			
+			if (con != null) {
+				con.close();
+			}
+		}
+		
+		return ;
+	    }
+	
+	
+
 
 	public User findByUserId(String userId) throws SQLException {
 		Connection con = null;
