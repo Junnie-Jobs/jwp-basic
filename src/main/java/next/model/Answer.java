12aa035d3db119669d503ad61f944c83ecb2b83f
Junnie-Jobs <dj0999@naver.com>
2016-03-21 08:12:38 +0900

    qna homework

---
+++ src/main/java/next/model/Answer.java
@@ -0,0 +1,91 @@
+package next.model;
+
+import java.sql.Timestamp;
+
+public class Answer {
+	
+	private long answerId;
+	private String writer;
+	private String contents;
+	private Timestamp createdDate;
+	private long questionId;
+	
+	public Answer(long answerId, String writer, String contents,
+			Timestamp createdDate, long questionId) {
+		this.answerId = answerId;
+		this.writer = writer;
+		this.contents = contents;
+		this.createdDate = createdDate;
+		this.questionId = questionId;
+	}
+
+	public long getAnswerId() {
+		return answerId;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+
+	public long getQuestionId() {
+		return questionId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (answerId ^ (answerId >>> 32));
+		result = prime * result + ((contents == null) ? 0 : contents.hashCode());
+		result = prime * result + ((createdDate == null) ? 0 : createdDate.hashCode());
+		result = prime * result + (int) (questionId ^ (questionId >>> 32));
+		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		if (answerId != other.answerId)
+			return false;
+		if (contents == null) {
+			if (other.contents != null)
+				return false;
+		} else if (!contents.equals(other.contents))
+			return false;
+		if (createdDate == null) {
+			if (other.createdDate != null)
+				return false;
+		} else if (!createdDate.equals(other.createdDate))
+			return false;
+		if (questionId != other.questionId)
+			return false;
+		if (writer == null) {
+			if (other.writer != null)
+				return false;
+		} else if (!writer.equals(other.writer))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Answer [answerId=" + answerId + ", writer=" + writer + ", contents=" + contents + ", createdDate="
+				+ createdDate + ", questionId=" + questionId + "]";
+	}
+
+}
